9b9d0061ae654ed77d81c08a2a72d37a854a5970
NikolayK <nikolay.kim@codeimmersive.com>
2020-04-16 23:59:54 -0400

    connects to backend and retrieves test message

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class App extends Component {
+   constructor(props) {
+      super(props)
+   
+      this.state = {
+         text: ''
+      }
+   }
+   
+   componentDidMount(){
+		axios.get(`/info`,).then((result) => {
+			this.setState({text: result.data.message})
+		})
+   }
+
+
    render() {
       return (
          <div>
-            <h1>Hello</h1>
+            <h1>{this.state.text}</h1>
          </div>
       )
    }
